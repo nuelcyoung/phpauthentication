5a25c1cadba847816aed7a665d3e34f3c87e88bb
KILOCHI <cnuel.young@gmail.com>
2021-04-16 10:00:33 +0100

    first commit

---
+++ process.php
@@ -0,0 +1,13 @@
+<?php
+if(isset(&_POST['login'])){
+    $user_name = $_POST['user_name'];
+    $password = $_POST['password'];
+}
+if(isset(&_POST['register'])){
+    $first_name =
+    $last_name =
+    $user_name =
+    $dob =
+    $password =
+}
+?>
