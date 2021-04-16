194cc7d1269a036c478804666484e69646846561
KILOCHI <cnuel.young@gmail.com>
2021-04-16 10:01:03 +0100

    first commit

--- process.php
+++ process.php
@@ -1,9 +1,9 @@
 <?php
-if(isset(&_POST['login'])){
+if(isset($_POST['login'])){
     $user_name = $_POST['user_name'];
     $password = $_POST['password'];
 }
-if(isset(&_POST['register'])){
+if(isset($_POST['register'])){
     $first_name =
     $last_name =
     $user_name =
---
+++ register.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Register</title>
+</head>
+<body>
+<h1>Register</h1>
+<form action="process.php" method="post">
+<input type="text" name="first_name" placeholder="Firstname">
+<input type="text" name="last_name" placeholder="Lastname">
+<input type="date" name="dob" placeholder="Date of Birth">
+<input type="text" name="user_name" placeholder="Username">
+<input type="password" name="password" placeholder="Password">
+<input type="submit" name="submit">
+</form>
+</body>
+</html>
